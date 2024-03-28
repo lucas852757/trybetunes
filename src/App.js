0044ee8fee2b1e1e43c264659fd78f81e9c2134d
Lucas <bithome1@outlook.com>
2024-03-27 21:44:17 -0300

    feat: Rotas  para a aplicação.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+// Rotas para a aplicação.
+
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
     // return (<p>TrybeTunes</p>);
     return (
       <Switch>
-        <Route path="/" component={ <Login /> } />
+        <Route exact path="/" component={ Login } />
+        <Route path="/search" component={ Search } />
+        <Route path="/album/:id" component={ Album } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route path="/profile/edit" component={ ProfileEdit } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="*" component={ NotFound } />
       </Switch>
     );
   }
