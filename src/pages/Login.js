322f4bb8f9aefc1e6841143d816d4094a72429c1
Lucas <bithome1@outlook.com>
2024-04-05 23:05:27 -0300

    feat: Formulário de identificação.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,87 @@
+/* eslint-disable no-magic-numbers */
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import * as services from '../services/userAPI';
+import MessageCharging from './MessageCharging';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      buttonValue: true,
+      value: '',
+      loading: false,
+      redirect: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    // this.handleSubmit = this.handleSubmit(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  handleClick() {
+    const { value } = this.state;
+    services.createUser({ name: value });
+
+    this.setState({
+      loading: true,
+    }, () => {
+      setTimeout(() => this.setState({ redirect: true }), 2000);
+    });
+  }
+
+  handleChange({ target }) {
+    const MAGICNUMBER = 3;
+    const { value } = target;
+
+    this.setState({
+      value,
+    }, () => {
+      if (value.length >= MAGICNUMBER) {
+        this.setState({
+          buttonValue: false,
+        });
+      } else {
+        this.setState(
+          {
+            buttonValue: true,
+          },
+        );
+      }
+    });
+  }
+
   render() {
+    const { buttonValue, value, loading, redirect } = this.state;
+
     return (
       <div data-testid="page-login">
         {/* <p>TrybeTunes</p> */}
+        <form onSubmit={ (event) => this.handleSubmit(event) }>
+          <label htmlFor="loginForm">
+            Name:
+            <input
+              data-testid="login-name-input"
+              onChange={ (event) => this.handleChange(event) }
+              value={ value }
+            />
+            <button
+              type="submit"
+              value="submit"
+              disabled={ buttonValue }
+              data-testid="login-submit-button"
+              onClick={ this.handleClick }
+              name="loginForm"
+            >
+              Entrar
+            </button>
+          </label>
+        </form>
+        {loading && <MessageCharging to="/search" />}
+        {redirect && <Redirect to="/search" />}
       </div>
     );
   }
---
+++ src/pages/MessageCharging.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Charging extends React.Component {
+  render() {
+    return (
+      <div>Carregando...</div>
+    );
+  }
+}
+
+export default Charging;
