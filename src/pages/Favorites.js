414ec853adee9f1b0e2f215d5abdc83c6b1422b2
Lucas <bithome1@outlook.com>
2024-04-29 13:02:23 -0300

    feat: Criando lista de músicas favoritas.

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import Header from '../components/Header';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import MessageCharging from './MessageCharging';
+import MusicCard from '../components/MusicCard';
 
 class Favorites extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: true,
+      requestFavoriteSongs: [],
+    };
+    this.changeStateFavoriteSongs = this.changeStateFavoriteSongs.bind(this);
+  }
+
+  async componentDidMount() {
+    const favortiteSongs = await getFavoriteSongs();
+    this.changeStateFavoriteSongs(favortiteSongs);
+  }
+
+  changeStateFavoriteSongs(arg) {
+    const MAGICNUMBER = 1000;
+    setTimeout(() => this.setState({
+      loading: false,
+      requestFavoriteSongs: [... arg],
+    }, MAGICNUMBER));
+  }
+
   render() {
+    const { loading } = this.state;
     return (
       <div data-testid="page-favorites">
         <Header />
+        {loading && <MessageCharging />}
       </div>);
   }
 }
