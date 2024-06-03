9b9643d07e45ec3b8732b32569e852e0c76008a4
Lucas <bithome1@outlook.com>
2024-06-02 23:04:05 -0300

    style: Cabeçalho.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import MessageCharging from '../pages/MessageCharging';
 import { getUser } from '../services/userAPI';
+import './header.css';
 
 class Header extends React.Component {
   constructor() {
@@ -28,17 +29,73 @@ class Header extends React.Component {
   render() {
     const { name, loading } = this.state;
     return (
-      <header data-testid="header-component">
+      <header className="grid-container" data-testid="header-component">
         {loading && <MessageCharging />}
-        <div data-testid="header-user-name">
-          {name}
+        <div className="header-top">
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+          />
+          <div className="header-top-mark">
+            trybe
+            {' '}
+            <span className="material-symbols-outlined material-icons md-18">
+              headphones
+            </span>
+            <div className="header-tunes">tunes</div>
+          </div>
+          <div className="header-user-name">
+            <span
+              className="material-symbols-outlined material-icons md-36"
+              style={ { float: 'left', paddingRight: '30px' } }
+            >
+              account_circle
+            </span>
+            <div
+              style={ {
+                float: 'right',
+                paddingRight: '30px',
+                paddingTop: '10px',
+              } }
+              data-testid="header-user-name"
+            >
+              {name}
+            </div>
+            <div />
+          </div>
         </div>
-        <Link to="/search" data-testid="link-to-search"> Pesquisa </Link>
-        <Link to="/favorites" data-testid="link-to-favorites">Favoritas</Link>
-        <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
+
+        {!loading && (
+          <>
+            <Link
+              className="header-search border"
+              to="/search"
+              data-testid="link-to-search"
+            >
+              {' '}
+              Pesquisa
+              {' '}
+            </Link>
+            <Link
+              className="header-favorite border"
+              to="/favorites"
+              data-testid="link-to-favorites"
+            >
+              Favoritas
+            </Link>
+            <Link
+              className="header-profile border"
+              to="/profile"
+              data-testid="link-to-profile"
+            >
+              Perfil
+            </Link>
+          </>
+        )}
       </header>
     );
   }
 }
 
 export default Header;
+
